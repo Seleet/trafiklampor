11de5abf32f7f505b3a32a469077765cb055b0d1
Martin Selin <seleeto@gmail.com>
2026-03-17 14:51:55 +0100

    state, current state,

--- index.js
+++ index.js
@@ -1,18 +1,48 @@
-function myLight(myColor) {
-  let redbutton = document.getElementById("myProgress-" + myColor);
+const LAMP = {
+  red: document.getElementById("myProgress-red"),
+  yellow: document.getElementById("myProgress-yellow"),
+  green: document.getElementById("myProgress-green"),
+};
 
-  switch (myColor) {
-    case "red":
-      redbutton.style.backgroundColor = myColor;
-      break;
-    case "yellow":
-      redbutton.style.backgroundColor = myColor;
-      break;
-    case "green":
-      redbutton.style.backgroundColor = myColor;
-      break;
+let currentState = "stop";
 
-    default:
-      redbutton.style.backgroundColor = "grey";
+function setLights(state) {
+  const grey = "grey";
+
+  if (state === "wait") {
+    if (currentState === "go") {
+      state = "waitToStop";
+    } else {
+      state = "waitToGo";
+    }
   }
+
+  switch (state) {
+    case "stop":
+      LAMP.red.style.backgroundColor = "red";
+      LAMP.yellow.style.backgroundColor = grey;
+      LAMP.green.style.backgroundColor = grey;
+      break;
+    case "waitToGo":
+      LAMP.red.style.backgroundColor = "red";
+      LAMP.yellow.style.backgroundColor = "yellow";
+      LAMP.green.style.backgroundColor = grey;
+      break;
+    case "waitToStop":
+      LAMP.red.style.backgroundColor = grey;
+      LAMP.yellow.style.backgroundColor = "yellow";
+      LAMP.green.style.backgroundColor = grey;
+      break;
+    case "go":
+      LAMP.red.style.backgroundColor = grey;
+      LAMP.yellow.style.backgroundColor = grey;
+      LAMP.green.style.backgroundColor = "green";
+      break;
+    default:
+      LAMP.red.style.backgroundColor = grey;
+      LAMP.yellow.style.backgroundColor = grey;
+      LAMP.green.style.backgroundColor = grey;
+  }
+
+  currentState = state;
 }
